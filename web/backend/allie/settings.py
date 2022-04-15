a9d8f8e706f2e7a2240a3c5dd07edc946f035567
VanshikaNehra23 <vanshika.nehra23@gmail.com>
2022-04-15 21:54:07 +0530

    fix: added corsheaders

--- web/backend/allie/settings.py
+++ web/backend/allie/settings.py
@@ -87,6 +87,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
